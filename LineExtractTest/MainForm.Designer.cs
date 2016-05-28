7943f14fabee65942abcb59a6ccdbb2005761740
IBUTA Kakeru <butameron@opap.jp>
2016-05-29 08:34:25 +0900

    first commit

---
+++ LineExtractTest/MainForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace LineExtractTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.threshold = new System.Windows.Forms.NumericUpDown();
+            this.hardwareInfoLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.processingTimeLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.threshold)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(15, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(85, 36);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "PNG|*.png";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "PNG|*.png";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.processingTimeLabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.threshold);
+            this.panel1.Controls.Add(this.hardwareInfoLabel);
+            this.panel1.Controls.Add(this.saveButton);
+            this.panel1.Controls.Add(this.startButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1208, 63);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Threshold";
+            // 
+            // threshold
+            // 
+            this.threshold.Location = new System.Drawing.Point(275, 12);
+            this.threshold.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.threshold.Name = "threshold";
+            this.threshold.Size = new System.Drawing.Size(55, 19);
+            this.threshold.TabIndex = 3;
+            this.threshold.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            // 
+            // hardwareInfoLabel
+            // 
+            this.hardwareInfoLabel.AutoSize = true;
+            this.hardwareInfoLabel.Location = new System.Drawing.Point(214, 36);
+            this.hardwareInfoLabel.Name = "hardwareInfoLabel";
+            this.hardwareInfoLabel.Size = new System.Drawing.Size(129, 12);
+            this.hardwareInfoLabel.TabIndex = 2;
+            this.hardwareInfoLabel.Text = "IsHardwareAccelerated: ";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(106, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(85, 36);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.mainPictureBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 63);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1208, 608);
+            this.panel2.TabIndex = 2;
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // processingTimeLabel
+            // 
+            this.processingTimeLabel.AutoSize = true;
+            this.processingTimeLabel.Location = new System.Drawing.Point(361, 14);
+            this.processingTimeLabel.Name = "processingTimeLabel";
+            this.processingTimeLabel.Size = new System.Drawing.Size(89, 12);
+            this.processingTimeLabel.TabIndex = 5;
+            this.processingTimeLabel.Text = "Processing time:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 671);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "LineExtractTest";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.threshold)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label hardwareInfoLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown threshold;
+        private System.Windows.Forms.Label processingTimeLabel;
+    }
+}
+
